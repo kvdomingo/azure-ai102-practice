60ef4146921caa68f8b4ad73c83506f177f5d0ae
Kenneth V. Domingo <hello@kvd.studio>
2024-05-26 17:15:23 +0800

    chore: add ruff

--- src/settings.py
+++ src/settings.py
@@ -4,10 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env',
-        env_file_encoding='utf-8',
+        env_file=".env",
+        env_file_encoding="utf-8",
         case_sensitive=True,
-        extra='ignore',
+        extra="ignore",
     )
 
     AZURE_AI_ENDPOINT: AnyHttpUrl
